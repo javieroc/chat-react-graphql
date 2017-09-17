a837d79295d1f3e14620ad5dca9bbd53daefb03f
Javier Ocampo <ferroxido@gmail.com>
2017-09-17 11:10:42 -0300

    Setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import Home from './screens/Home'
+import Login from './screens/Login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/login" component={Login}/>
+        </div>
+      </Router>
     )
   }
 }
