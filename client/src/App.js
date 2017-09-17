e5b1e76a82dcdcdd58001eaab9c7770cd2f8e76c
Javier Ocampo <ferroxido@gmail.com>
2017-09-17 12:01:47 -0300

    Add Nav component

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,14 @@ import {
 
 import Home from './screens/Home'
 import Login from './screens/Login'
+import Nav from './components/Nav'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/login">Login</Link></li>
-          </ul>
-
-          <hr/>
-
+          <Nav />
           <Route exact path="/" component={Home}/>
           <Route path="/login" component={Login}/>
         </div>
