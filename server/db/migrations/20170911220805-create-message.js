27d43f4918beebf182f53f6079b7c8edde630532
Javier Ocampo <ferroxido@gmail.com>
2017-10-02 10:28:21 -0300

    Fix column names to underscore

--- server/db/migrations/20170911220805-create-message.js
+++ server/db/migrations/20170911220805-create-message.js
@@ -26,11 +26,11 @@ module.exports = {
         key: 'id'
       }
     },
-    createdAt: {
+    created_at: {
       allowNull: false,
       type: Sequelize.DATE
     },
-    updatedAt: {
+    updated_at: {
       allowNull: false,
       type: Sequelize.DATE
     }
--- server/db/migrations/20170912012408-create-room-user.js
+++ server/db/migrations/20170912012408-create-room-user.js
@@ -25,11 +25,11 @@ module.exports = {
         key: 'id'
       }
     },
-    createdAt: {
+    created_at: {
       allowNull: false,
       type: Sequelize.DATE
     },
-    updatedAt: {
+    updated_at: {
       allowNull: false,
       type: Sequelize.DATE
     }
