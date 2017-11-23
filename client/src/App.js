468973e7c7623d2114c71d04fdb24449b91f9427
Javier Ocampo <ferroxido@gmail.com>
2017-11-22 22:07:58 -0300

    Add firebase to client side

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,19 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
+import firebase from 'firebase';
 import Routes from './routes';
 import './App.css';
+
+const config = {
+  apiKey: 'AIzaSyDuX-5ZIA8Bu_vtND1XazSVRf1FYs_yMy8',
+  authDomain: 'chat-react-graphql.firebaseapp.com',
+  databaseURL: 'https://chat-react-graphql.firebaseio.com',
+  projectId: 'chat-react-graphql',
+  storageBucket: 'chat-react-graphql.appspot.com',
+};
+
+firebase.initializeApp(config);
 
 const client = new ApolloClient({
   link: createHttpLink({ uri: '/api/graphql' }),
