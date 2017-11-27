d7aaa8ea7aa83373c94ba7a8e60aba449f05c3b3
Javier Ocampo <ferroxido@gmail.com>
2017-11-27 14:42:19 -0300

    Setup graphql subscriptions on server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
+import { createServer } from 'http';
+import { execute, subscribe } from 'graphql';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import schema from './schema';
 import config from './config';
 import app from './app';
 
+const server = createServer(app);
 const { port } = config;
 
-app.listen(port, () => {
+server.listen(port, () => {
+  // eslint-disable-next-line no-new
+  new SubscriptionServer({
+    execute,
+    subscribe,
+    schema,
+  }, {
+    server,
+    path: '/subscriptions',
+  });
   console.log(`Server running on http://localhost:${port}`);
 });
