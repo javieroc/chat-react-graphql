e0c3fc3ae5c6d46bb68b24df6db45584b65dcd24
Javier Ocampo <ferroxido@gmail.com>
2017-11-29 10:54:55 -0300

    Protect room route

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,22 +2,58 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
+  Redirect,
 } from 'react-router-dom';
+import decode from 'jwt-decode';
 import ViewRoom from './ViewRoom';
 import Login from './Login';
 import Register from './Register';
 import Nav from '../components/Nav';
 import Home from './Home';
 
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  try {
+    decode(token);
+    decode(refreshToken);
+  } catch (err) {
+    return false;
+  }
+
+  return true;
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (
+    isAuthenticated() ? (
+      <Component {...props} />
+    ) : (
+      <Redirect
+        to={{
+          pathname: '/login',
+          state: { from: props.location },
+        }}
+      />
+    )
+  )}
+  />
+);
+
 const Routes = () => (
   <Router>
     <div>
       <Nav />
       <br />
-      <Route path="/" exact component={Home} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/register" exact component={Register} />
-      <Route path="/rooms/:roomId?" component={ViewRoom} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <PrivateRoute path="/rooms/:roomId?" component={ViewRoom} />
+      </Switch>
     </div>
   </Router>
 );
